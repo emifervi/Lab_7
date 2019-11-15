cbc14fcfa69d1b67ac3f7e0ade7c59cb2f25c5f6
emifervi <emifervi@hotmail.com>
2019-11-14 19:46:19 -0600

    hide db pass

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-module.exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://m001-student:SgagdC9B.@sandbox-kxnp6.mongodb.net/characters?retryWrites=true&w=majority";
+const DB_PASSWORD = require('./db/dbCredentials').DB_PASSWORD;
+
+module.exports.DATABASE_URL = process.env.DATABASE_URL || `mongodb+srv://m001-student:${DB_PASSWORD}@sandbox-kxnp6.mongodb.net/characters?retryWrites=true&w=majority`;
 module.exports.PORT = process.env.PORT || 8080;
