b00b33e7c0d2980a7a560d5712b04c8f25b4d079
emifervi <emifervi@hotmail.com>
2019-11-14 22:47:21 -0600

    change config credentials

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
-const DB_PASSWORD = require('./db/dbCredentials').DB_PASSWORD;
+var DATABASE_URL, PORT;
 
-module.exports.DATABASE_URL = process.env.DATABASE_URL || `mongodb+srv://m001-student:${DB_PASSWORD}@sandbox-kxnp6.mongodb.net/characters?retryWrites=true&w=majority`;
-module.exports.PORT = process.env.PORT || 8080;
+if(process.env.NODE_ENV === 'production'){
+  DATABASE_URL = process.env.DATABASE_URL,
+  PORT = process.env.PORT;
+} else {
+  const DB_PASSWORD = require('./db/dbCredentials').DB_PASSWORD;
+  DATABASE_URL = `mongodb+srv://m001-student:${DB_PASSWORD}@sandbox-kxnp6.mongodb.net/characters?retryWrites=true&w=majority`;
+  PORT = 8080;
+}
+
+module.exports = {
+  DATABASE_URL,
+  PORT
+}
